0ba1023a32b275cbfa94387aad28b9020d7cdef2
Revvrel <khairul.m@hotmail.com>
2024-01-19 23:06:37 +0800

    Change file name

--- src/test/java/com/example/librarybookingsystem/LoanPeriodControllerTest.java
+++ src/test/java/com/example/librarybookingsystem/LoanPeriodControllerTest.java
@@ -20,7 +20,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class LoadPeriodControllerTest {
+public class LoanPeriodControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
